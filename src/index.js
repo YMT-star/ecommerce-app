de68694a9d24d312c32427ef1e3eea2d87af0bb9
YE MIN TUN <tun700632@gmail.com>
2023-12-03 22:32:04 +0630

    change css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
-// import "./index.css";
+import "./index.css";
 import App from "./App";
 // import reportWebVitals from './reportWebVitals';
 
